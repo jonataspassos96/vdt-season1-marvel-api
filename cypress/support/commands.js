33401d300fe0e8f073d48c18fc9d8599360f3ee7
Jonatas Passos <jpc080496@gmail.com>
2022-08-01 18:59:13 -0300

    feat: implemented custom commands, setToken and back2ThePast

---
+++ cypress/support/commands.js
@@ -0,0 +1,62 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+/* Cypress.Commands.add('resetCharacters', (id) => {
+  cy.request({
+    method: 'DELETE',
+    url: `/back2thepast/${id}`
+  }).then(response => {
+    expect(response.status).be.equal(200);
+  })
+})
+ */
+
+Cypress.Commands.add('setToken', () => {
+  cy.request({
+    method: 'POST',
+    url: '/sessions',
+    body: {
+      email: 'jonatas@qacademy.io',
+      password: 'qa-cademy'
+    }
+  }).then(response => {
+    expect(response.status).be.eq(200)
+    cy.log(response.body.token)
+    cy.log(response.body.user._id)
+
+    Cypress.env('token', response.body.token)
+    Cypress.env('id', response.body.user._id)
+  })
+})
+
+Cypress.Commands.add('back2ThePast', () => {
+  cy.request({
+    method: 'DELETE',
+    url: `/back2thepast/${Cypress.env('id')}`
+  }).then(response => {
+    expect(response.status).be.equal(200);
+  })
+})
