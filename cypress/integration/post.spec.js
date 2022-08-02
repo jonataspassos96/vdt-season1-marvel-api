14d4fb4b7497019b17b8f3ff6faeb22bf523ae1e
Jonatas Passos <jpc080496@gmail.com>
2022-08-02 09:40:17 -0300

    tests:  All fields are mandatory

--- cypress/integration/post.spec.js
+++ cypress/integration/post.spec.js
@@ -49,4 +49,29 @@ describe('POST /characters', () => {
 
     })
 
+    context('todos os campos são obrigatórios', () => {
+        const character = {
+            name: 'Charles Xavier',
+            alias: 'Professor X',
+            team: ['x-men', 'illuminatis'],
+            active: true
+        }
+
+        const fields = ['name', 'alias', 'team', 'active']
+
+        fields.forEach(field => {
+            it(`campo ${field} é obrigatório`, () => {
+                const copy = JSON.parse(JSON.stringify(character))
+
+                delete copy[field]
+
+                cy.postCharacter(copy)
+                    .then(res => {
+                        expect(res.status).to.eq(400)
+                        expect(res.body.validation.body.message).deep.include(field)
+                    })
+            })
+        })
+    })
+
 })
